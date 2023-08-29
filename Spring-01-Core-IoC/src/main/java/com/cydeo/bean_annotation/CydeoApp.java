87532fd1c6ee2553de389dc2070db4786f90f8f5
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2023-08-29 19:57:53 -0400

    Bean Annotation

---
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/CydeoApp.java
@@ -0,0 +1,24 @@
+package com.cydeo.bean_annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CydeoApp {
+    public static void main(String[] args) {
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class, ConfigAny.class);
+
+       FullTimeMentor ft = container.getBean(FullTimeMentor.class);
+       ft.createAccount();
+       PartTimeMentor pt = container.getBean(PartTimeMentor.class);
+       pt.createAccount();
+
+       String st = container.getBean(String.class);
+        System.out.println(st);
+
+
+
+
+
+    }
+}
---
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/FullTimeMentor.java
@@ -0,0 +1,10 @@
+package com.cydeo.bean_annotation;
+
+import org.springframework.context.annotation.Bean;
+
+public class FullTimeMentor {
+
+    public void createAccount(){
+        System.out.println("Full Time Mentor is created");
+    }
+}
