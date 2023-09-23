f3ae8eff3d85ff91f45e735186751cec3ce7228e
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2023-09-22 21:02:46 -0400

    Spring Thymeleaf

--- Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,17 +3,20 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
-    @RequestMapping("/register")
+//    @RequestMapping("/register")
+    @GetMapping("/register")
     public String register(Model model){
         model.addAttribute("students", DataGenerator.createStudent());
         return "student/register";
     }
-    @RequestMapping("/welcome")
+//    @RequestMapping("/welcome")
+    @GetMapping("/welcome")
     public String info(){
         return "student/welcome";
     }
