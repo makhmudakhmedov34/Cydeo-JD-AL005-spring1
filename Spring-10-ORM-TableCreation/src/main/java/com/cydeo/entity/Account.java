d08e6edc2fd0c787585903341d1540a4b1650a4e
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2023-10-18 20:05:32 -0400

    ORM Part 2

--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Account.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Account.java
@@ -1,9 +1,12 @@
 package com.cydeo.entity;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 
+@MappedSuperclass
 public class Account {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String owner;
     private BigDecimal balance;
