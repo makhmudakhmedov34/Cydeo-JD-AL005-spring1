50e3ad8cc04bdbe1c656d152dc769d96086d04df
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2023-10-30 19:31:05 -0400

    Many to One

--- Spring-12-ORM-OneToMany-ManyTo/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-12-ORM-OneToMany-ManyTo/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,8 +1,12 @@
 package com.cydeo.bootstrap;
 
+import com.cydeo.entity.Customer;
+import com.cydeo.entity.Merchant;
 import com.cydeo.entity.Payment;
 import com.cydeo.entity.PaymentDetail;
 import com.cydeo.enums.Status;
+import com.cydeo.repository.CustomerRepository;
+import com.cydeo.repository.MerchantRepository;
 import com.cydeo.repository.PaymentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,9 +17,13 @@ import java.time.LocalDate;
 public class DataGenerator implements CommandLineRunner {
 
     PaymentRepository paymentRepository;
+    private final MerchantRepository merchantRepository;
+    private final CustomerRepository customerRepository;
 
-    public DataGenerator(PaymentRepository paymentRepository) {
+    public DataGenerator(PaymentRepository paymentRepository, MerchantRepository merchantRepository, CustomerRepository customerRepository) {
         this.paymentRepository = paymentRepository;
+        this.merchantRepository = merchantRepository;
+        this.customerRepository = customerRepository;
     }
 
     @Override
@@ -27,6 +35,21 @@ public class DataGenerator implements CommandLineRunner {
         Payment payment2 = new Payment(LocalDate.of(2022,4,25),new BigDecimal("100000"), Status.FAILURE);
         PaymentDetail paymentDetail2 = new PaymentDetail(new BigDecimal("90000"),new BigDecimal("5000"),LocalDate.of(2022,4,29));
         payment2.setPaymentDetail(paymentDetail2);
+
+        Merchant merchant1 = new Merchant("AmazonSubMerchant","M123",new BigDecimal("0.25"),new BigDecimal("3.25"),5);
+
+        Customer customer1 = new Customer("msmith","Mike","Smith","msmith@cydeo.com","VA");
+
+        payment1.setCustomer(customer1);
+        payment2.setCustomer(customer1);
+        customerRepository.save(customer1);
+
+
+        payment1.setMerchant(merchant1);
+        payment2.setMerchant(merchant1);
+
+
+        merchantRepository.save(merchant1);
 
         paymentRepository.save(payment1);
         paymentRepository.save(payment2);
--- Spring-12-ORM-OneToMany-ManyTo/src/main/java/com/cydeo/entity/Payment.java
+++ Spring-12-ORM-OneToMany-ManyTo/src/main/java/com/cydeo/entity/Payment.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name="payments")
@@ -22,6 +23,17 @@ public class Payment {
     private Status paymentStatus;
     @OneToOne(cascade = CascadeType.ALL)
     private PaymentDetail paymentDetail;
+
+
+    @ManyToOne()
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "merchant_id")
+    private Merchant merchant;
+
+
 
     public Payment(LocalDate createDate, BigDecimal amount, Status paymentStatus) {
         this.createDate = createDate;
