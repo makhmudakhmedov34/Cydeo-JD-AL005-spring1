c8667667a108262a5351d31487e62cbc23abd7d4
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2023-12-11 22:43:14 -0500

    AOP

---
+++ Spring-20-AOP/src/main/java/com/cydeo/Spring20AopApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring20AopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring20AopApplication.class, args);
+    }
+
+}
---
+++ Spring-20-AOP/src/main/java/com/cydeo/entity/Course.java
@@ -0,0 +1,30 @@
+package com.cydeo.entity;
+
+import javax.persistence.*;
+
+
+import lombok.Data;
+
+@Data
+
+@Table(name = "COURSES")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "CATEGORY")
+    private String category;
+
+    @Column(name = "RATING")
+    private int rating;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+}
