becbf02ffb3dd5ec56d0ee7669b974ae8b407e22
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2023-12-26 19:56:24 -0500

    AOP

---
+++ Spring-20-AOP/src/test/java/com/cydeo/spring20aop/Spring20AopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring20aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring20AopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
